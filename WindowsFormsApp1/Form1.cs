54482f650f11bec4360b9c6b825d45b9ebf9bba1
Dmitry <DiordiychukDmitry@gmail.com>
2019-10-15 16:31:30 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load( object sender, EventArgs e )
+        {
+
+        }
+
+        private void openFileToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "object files (*.obj)|*.obj|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //получить путь
+                    filePath = openFileDialog.FileName; //Нужно вернуть
+                }
+            }
+        }
+    }
+}
